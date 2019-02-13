d655fea74b97ce0c91844bb2a93a475c7e1ee9b8
SudarshanChowdhary <sudarshan@gmail.com>
2019-02-14 01:48:22 +0530

    worked on initially checkbox disable and after success enable

--- js/background.js
+++ js/background.js
@@ -2,15 +2,17 @@ var checkboxEle = document.getElementById("mat-checkbox-4-input");
 console.log(checkboxEle);
 checkboxEle.setAttribute("disabled", true);
 
-alert("background")
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
+                var checkboxEle = document.getElementById("mat-checkbox-4-input");
+                console.log(checkboxEle);
+//                checkboxEle.setAttribute("disabled", false);
+                checkboxEle.removeAttribute("disabled", true);
+
     if (request.greeting == "hello"){
-      checkboxEle.setAttribute("disabled", false);
       sendResponse({farewell: "goodbye"});
     }
 
