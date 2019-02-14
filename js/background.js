331dc1ad8147157f8986027d79ee00c9f01a009b
Koyalkar <Goverdhan.Koyalkar@searshc.com>
2019-02-15 01:21:59 +0530

    added jquery

--- js/background.js
+++ js/background.js
@@ -1,18 +1,37 @@
-var checkboxEle = document.getElementById("mat-checkbox-4-input");
-console.log(checkboxEle);
-checkboxEle.setAttribute("disabled", true);
+// // var checkboxEle = document.getElementById("mat-checkbox-4-input");
+// // console.log(checkboxEle);
+// // checkboxEle.setAttribute("disabled", true);
+
+// var checkboxEle = document.querySelectorAll("[id='mat-checkbox-4-input']");
+// // var checkboxEle = document.getElementById("mat-checkbox-4-input");
+
+// for(var i=0; i<checkboxEle.length; i++){
+//   console.log(checkboxEle[i]);
+//   checkboxEle[i].setAttribute("disabled", true);
+// }
+
+$(document).ready(function(){
+  alert("background");
+  $("#mat-checkbox-4-input").prop('disabled', true);
+})
+
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-                var checkboxEle = document.getElementById("mat-checkbox-4-input");
-                console.log(checkboxEle);
-//                checkboxEle.setAttribute("disabled", false);
-                checkboxEle.removeAttribute("disabled", true);
+//                 var checkboxEle = document.getElementById("mat-checkbox-4-input");
+//                 console.log(checkboxEle);
+// //                checkboxEle.setAttribute("disabled", false);
+//                 checkboxEle.removeAttribute("disabled", true);
 
+
+
+alert("background")
     if (request.greeting == "hello"){
+      $("#mat-checkbox-4-input").prop('disabled', false);
       sendResponse({farewell: "goodbye"});
     }
 
