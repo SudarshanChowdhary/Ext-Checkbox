d2413382ac4ee1c01c128fb07bd7a4becef40248
Koyalkar <Goverdhan.Koyalkar@searshc.com>
2019-02-15 10:42:27 +0530

    chrome executescript

--- js/background.js
+++ js/background.js
@@ -1,7 +1,31 @@
-$(document).ready(function () {
-  alert("background");
-  $("#mat-checkbox-4-input").prop('disabled', true);
-})
+// $(document).ready(function () {
+//   alert("background");
+//   $("#mat-checkbox-4-input").prop('disabled', true);
+// })
+
+function modifyDOM1() {
+  var keywords = [];
+  console.log("hello modifydom1");
+  var checkboxEle = document.getElementById("mat-checkbox-4-input");
+  console.log(checkboxEle);
+  checkboxEle.setAttribute("disabled", true);
+  return keywords;
+}
+
+function modifyDOM() {
+  var keywords = [];
+  console.log("hello modifydom1");
+  var checkboxEle = document.getElementById("mat-checkbox-4-input");
+  console.log(checkboxEle);
+  checkboxEle.removeAttribute("disabled", true);
+  return keywords;
+}
+chrome.tabs.executeScript({
+  code: '(' + modifyDOM1 + ')();' //argument here is a string but function.toString() returns function's code
+}, (results) => {
+  //Here we have just the innerHTML and not DOM structure
+});
+
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
@@ -10,9 +34,16 @@ chrome.runtime.onMessage.addListener(
       "from the extension");
 
     if (request.greeting == "hello") {
-      $("#mat-checkbox-4-input").prop('disabled', false);
+      //      $("#mat-checkbox-4-input").prop('disabled', false);
+
+      chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+      }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+      });
+
       sendResponse({
         farewell: "goodbye"
       });
     }
-});
+  });
